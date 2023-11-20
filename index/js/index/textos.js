639644ea2ed58802d57d225f33357733b27ddc99
Lestic <83370672+Lesticc@users.noreply.github.com>
2023-11-20 07:33:49 -0300

    Mais textos e 1 fórum
    Textos que aparecerem na index.html e o fórum "Comunidade Tóxica"

--- index/js/index/textos.js
+++ index/js/index/textos.js
@@ -2,5 +2,7 @@ let frases = [
   "Lestic vem de Leusatico, que vem de Leus, que vem de Meleus, que vem de Medeus. (Sim, eu Lestic era meio doido, falava tudo junto)",
   "Caso você esteja com preguiça de ler, simplesmente não leia, espero que eu tenha ajudado.",
   "Uma coisa sobre mim Lestic, é que cansado minha eficiência não diminui em 10%, diminui em 50%",
-  "O nome do dono do site (no caso o próprio Lestic) é Miguel."
+  "Ainda bem que não canso tão fácil..",
+  "O nome do dono do site (no caso o próprio Lestic) é Miguel.",
+  "Eu me inspirei para fazer um site no site do caua, dê uma passada lá :D https://thecauagames.github.io/"
   ]
